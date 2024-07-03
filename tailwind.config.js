1953d83c4f553b35d2ad0bae75f252899f61c4be
Stéphanie Vanoverberghe <orangestreet59123@gmail.com>
2024-07-03 17:19:40 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html, js}",
+    "./*.{html, js}",
+    "./pages/*.{html, js}"
+  ],
+  theme: {
+    // A l'extérieur, ça remplace les fonctionnalités se trouvant dans le thème tailwind
+    extend: {
+      // A l'intérieur, ça ajoute des fonctionnalités au thème tailwind sans rien écraser
+    },
+  },
+  plugins: [],
+}
+
