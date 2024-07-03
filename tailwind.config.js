04e4d3b210104d5d779a1345acf3b956ff38941c
Stéphanie Vanoverberghe <orangestreet59123@gmail.com>
2024-07-03 18:09:18 +0200

    fix: add font, files JS, img + config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -9,6 +11,18 @@ module.exports = {
     // A l'extérieur, ça remplace les fonctionnalités se trouvant dans le thème tailwind
     extend: {
       // A l'intérieur, ça ajoute des fonctionnalités au thème tailwind sans rien écraser
+      colors: {
+        'color-site': {
+          50: '#ffd15b',
+          100: '#1b1b1b',
+          200: '#ededed',
+          300: '#7a7a7a'
+        }
+      },
+      fontFamily: {
+        anton: ['Anton', 'sans-serif'],
+        manrope: ['Manrope', 'sans-serif'],
+      },
     },
   },
   plugins: [],
