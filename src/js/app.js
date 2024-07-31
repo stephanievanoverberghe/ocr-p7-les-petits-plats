7133ae7e3460b2bf52001f3bc5ab0dc12c5e27ea
stéphanie vanoverberghe <orangestreet59123@gmail.com>
2024-07-31 09:11:09 +0200

    feat: add search fonctionnality

--- src/js/app.js
+++ src/js/app.js
@@ -1,273 +1,264 @@
-import { Api } from "./api/api.js";
+import { fetchRecipes } from "./api/api.js";
 import { Recipe } from "./models/recipe.js";
+
+// Global variables to hold recipes and selected items
+let recipes = [];
+let selectedItems = {
+    ingredients: new Set(),
+    appliances: new Set(),
+    ustensils: new Set()
+};
 
 /**
  * Escape HTML characters to prevent XSS attacks.
  * @param {string} unsafe - The unsafe string to escape.
  * @returns {string} - The escaped string.
  */
-const escapeHtml = (unsafe) => {
-    return unsafe
-        .replace(/&/g, "&amp;")
-        .replace(/</g, "&lt;")
-        .replace(/>/g, "&gt;")
-        .replace(/"/g, "&quot;")
-        .replace(/'/g, "&#039;");
+const escapeHtml = unsafe => {
+    const map = {
+        '&': '&amp;',
+        '<': '&lt;',
+        '>': '&gt;',
+        '"': '&quot;',
+        "'": '&#039;'
+    };
+    return unsafe.replace(/[&<>"']/g, m => map[m]);
 };
 
 /**
- * Perform a search on recipes using functional programming methods.
+ * Perform a search on recipes using functional programming.
  * @param {string} query - The search query.
  * @param {Array} recipes - The list of recipes.
  * @returns {Array} - The filtered list of recipes.
  */
 const searchRecipesFunctional = (query, recipes) => {
     const lowerCaseQuery = query.toLowerCase();
-    return recipes.filter(recipe => matchesQueryFunctional(lowerCaseQuery, recipe));
+    return recipes.filter(recipe => matchesQuery(lowerCaseQuery, recipe));
 };
 
 /**
- * Check if a recipe matches the search query using functional programming methods.
+ * Check if a recipe matches the search query.
  * @param {string} query - The search query.
  * @param {Object} recipe - The recipe object.
  * @returns {boolean} - True if the recipe matches the query, false otherwise.
  */
-const matchesQueryFunctional = (query, recipe) => {
+const matchesQuery = (query, recipe) => {
     return recipe.name.toLowerCase().includes(query) ||
         recipe.ingredients.map(ing => ing.ingredient.toLowerCase()).join(' ').includes(query) ||
         recipe.description.toLowerCase().includes(query);
 };
 
 /**
- * Check if a recipe matches the selected items using functional programming methods.
+ * Check if a recipe matches the selected items.
  * @param {Object} recipe - The recipe object.
  * @param {Object} selectedItems - The selected items.
  * @returns {boolean} - True if the recipe matches the selected items, false otherwise.
  */
-const matchesSelectedItemsFunctional = (recipe, selectedItems) => {
-    const ingredientMatch = [...selectedItems.ingredients].every(item =>
-        recipe.ingredients.some(ing => ing.ingredient.toLowerCase() === item.toLowerCase())
-    );
-
-    const applianceMatch = selectedItems.appliances.size === 0 ||
-        selectedItems.appliances.has(recipe.appliance.toLowerCase());
-
-    const ustensilMatch = [...selectedItems.ustensils].every(item =>
-        recipe.ustensils.some(ust => ust.toLowerCase() === item.toLowerCase())
-    );
-
-    return ingredientMatch && applianceMatch && ustensilMatch;
+const matchesSelectedItems = (recipe, selectedItems) => {
+    return matchesAllItems(recipe.ingredients, selectedItems.ingredients, 'ingredient') &&
+        matchesSingleItem(recipe.appliance, selectedItems.appliances) &&
+        matchesAllItems(recipe.ustensils, selectedItems.ustensils);
 };
 
-class App {
-    constructor() {
-        this.api = new Api('src/data/recipes.json');
-        this.selectedItems = {
-            ingredients: new Set(),
-            appliances: new Set(),
-            ustensils: new Set()
-        };
-    }
+const matchesAllItems = (recipeItems, selectedItems, key) => {
+    return Array.from(selectedItems).every(item =>
+        recipeItems.some(ing => (key ? ing[key] : ing).toLowerCase() === item.toLowerCase())
+    );
+};
 
-    /**
-     * Initialize the application and load recipes.
-     */
-    async main() {
-        try {
-            const data = await this.api.get();
+const matchesSingleItem = (recipeItem, selectedItems) => {
+    return selectedItems.size === 0 || selectedItems.has(recipeItem.toLowerCase());
+};
 
-            if (data && data.recipes) {
-                this.recipes = data.recipes.map(recipe => new Recipe(recipe));
-                this.updateRecipeCount(this.recipes.length);
-                this.displayRecipes(this.recipes);
-                this.populateSelects();
+/**
+ * Fetch and initialize recipes.
+ */
+const main = async () => {
+    try {
+        const data = await fetchRecipes('src/data/recipes.json');
 
-                document.querySelector('#search').addEventListener('input', (event) => this.filterRecipes(event.target.value));
-                document.querySelector('#clear-button').addEventListener('click', () => {
-                    document.querySelector('#search').value = '';
-                    this.filterRecipes('');
-                });
+        if (data && data.recipes) {
+            recipes = data.recipes.map(recipe => new Recipe(recipe));
+            updateRecipeCount(recipes.length);
+            displayRecipes(recipes);
+            populateSelects();
 
-                this.addSelectEventListeners();
-            } else {
-                console.error('No recipes found in the data');
-            }
-        } catch (error) {
-            console.error('Error in main:', error);
+            document.querySelector('#search').addEventListener('input', (event) => filterRecipes(event.target.value));
+            document.querySelector('#clear-button').addEventListener('click', () => {
+                document.querySelector('#search').value = '';
+                filterRecipes('');
+            });
+
+            addSelectEventListeners();
+        } else {
+            console.error('No recipes found in the data');
         }
+    } catch (error) {
+        console.error('Error in main:', error);
     }
+};
 
-    /**
-     * Display the recipes in the DOM.
-     * @param {Array} recipes - The list of recipes to display.
-     */
-    displayRecipes(recipes) {
-        const recipesContainer = document.querySelector('#recipes-container');
-        recipesContainer.innerHTML = '';
+/**
+ * Display recipes in the DOM.
+ * @param {Array} recipes - The list of recipes to display.
+ */
+const displayRecipes = recipes => {
+    const recipesContainer = document.querySelector('#recipes-container');
+    recipesContainer.innerHTML = recipes.map(recipe => createRecipeCard(recipe)).join('');
+};
 
-        recipes.forEach(recipe => {
-            const recipeCard = document.createElement('article');
-            recipeCard.classList.add('relative', 'flex', 'flex-col', 'overflow-hidden', 'rounded-3xl', 'bg-white', 'shadow-lg');
-            recipeCard.innerHTML = `
-          <img src="src/assets/img/${recipe.image}" alt="${escapeHtml(recipe.name)}" class="h-64 w-full object-cover"/>
-          <div class="absolute right-4 top-4 rounded-xl bg-yellow-400 px-4 py-1 text-xs text-color-site-100">${recipe.time}min</div>
-          <div class="flex flex-grow flex-col px-6 pb-16 pt-8">
+const createRecipeCard = recipe => `
+    <article class="relative flex flex-col overflow-hidden rounded-3xl bg-white shadow-lg">
+        <img src="src/assets/img/${recipe.image}" alt="${escapeHtml(recipe.name)}" class="h-64 w-full object-cover"/>
+        <div class="absolute right-4 top-4 rounded-xl bg-yellow-400 px-4 py-1 text-xs text-color-site-100">${recipe.time}min</div>
+        <div class="flex flex-grow flex-col px-6 pb-16 pt-8">
             <h2 class="mb-7 font-anton text-lg font-bold">${escapeHtml(recipe.name)}</h2>
             <h3 class="mb-2 text-sm font-bold uppercase tracking-wide text-color-site-300">Recette</h3>
-            <p class="mb-8 flex-grow text-gray-700 line-clamp-4">${escapeHtml(this.truncateDescription(recipe.description))}</p>
+            <p class="mb-8 flex-grow text-gray-700 line-clamp-4">${escapeHtml(truncateDescription(recipe.description))}</p>
             <h3 class="mb-4 text-sm font-bold uppercase tracking-wide text-color-site-300">Ingrédients</h3>
             <div class="grid grid-cols-2 gap-y-5">
-              ${recipe.ingredients.map(ingredient => `
-                <div class="flex flex-col text-sm">
-                  <span class="font-medium">${escapeHtml(ingredient.ingredient)}</span>
-                  <span class="text-color-site-300">${ingredient.quantity || ''} ${ingredient.unit || ''}</span>
-                </div>
-              `).join('')}
+                ${recipe.ingredients.map(ingredient => `
+                    <div class="flex flex-col text-sm">
+                        <span class="font-medium">${escapeHtml(ingredient.ingredient)}</span>
+                        <span class="text-color-site-300">${ingredient.quantity || ''} ${ingredient.unit || ''}</span>
+                    </div>
+                `).join('')}
             </div>
-          </div>
-        `;
-            recipesContainer.appendChild(recipeCard);
-        });
+        </div>
+    </article>
+`;
+
+/**
+ * Filter the recipes based on the search query and selected items.
+ * @param {string} query - The search query.
+ */
+const filterRecipes = query => {
+    const filteredRecipes = searchRecipesFunctional(query, recipes)
+        .filter(recipe => matchesSelectedItems(recipe, selectedItems));
+    updateRecipeCount(filteredRecipes.length);
+    displayRecipes(filteredRecipes);
+};
+
+/**
+ * Update the recipe count display.
+ * @param {number} count - The number of recipes.
+ */
+const updateRecipeCount = count => {
+    const recipeCountElement = document.querySelector('#recipe-count');
+    let countText;
+
+    if (count === 0) {
+        countText = '00 recette';
+    } else if (count === 1) {
+        countText = '01 recette';
+    } else {
+        countText = `${count.toString().padStart(2, '0')} recettes`;
     }
 
-    /**
-     * Filter the recipes based on the search query and selected items using functional methods.
-     * @param {string} query - The search query.
-     */
-    filterRecipes(query) {
-        const filteredRecipes = searchRecipesFunctional(query, this.recipes)
-            .filter(recipe => matchesSelectedItemsFunctional(recipe, this.selectedItems));
-        this.updateRecipeCount(filteredRecipes.length);
-        this.displayRecipes(filteredRecipes);
-    }
+    recipeCountElement.textContent = countText;
+};
 
-    /**
-     * Update the displayed recipe count.
-     * @param {number} count - The number of recipes.
-     */
-    updateRecipeCount(count) {
-        const recipeCountElement = document.querySelector('#recipe-count');
-        recipeCountElement.textContent = count === 0 ? '00 recette' : `${count.toString().padStart(2, '0')} recettes`;
-    }
+/**
+ * Truncate the description to a specified number of lines.
+ * @param {string} description - The description to truncate.
+ * @param {number} [maxLines=4] - The maximum number of lines.
+ * @returns {string} - The truncated description.
+ */
+const truncateDescription = (description, maxLines = 4) => {
+    const lines = description.split('\n');
+    return lines.length <= maxLines ? description : lines.slice(0, maxLines).join(' ') + '...';
+};
 
-    /**
-     * Truncate the description to a specified number of lines.
-     * @param {string} description - The description to truncate.
-     * @param {number} [maxLines=4] - The maximum number of lines.
-     * @returns {string} - The truncated description.
-     */
-    truncateDescription(description, maxLines = 4) {
-        const lines = description.split('\n');
-        return lines.length <= maxLines ? description : lines.slice(0, maxLines).join(' ') + '...';
-    }
+/**
+ * Populate the select elements with ingredients, appliances, and utensils.
+ */
+const populateSelects = () => {
+    const ingredients = new Set();
+    const appliances = new Set();
+    const ustensils = new Set();
 
-    /**
-     * Populate the selects with ingredients, appliances, and ustensils.
-     */
-    populateSelects() {
-        const ingredients = new Set();
-        const appliances = new Set();
-        const ustensils = new Set();
+    recipes.forEach(recipe => {
+        addItemsToSet(ingredients, recipe.ingredients, 'ingredient');
+        appliances.add(recipe.appliance);
+        addItemsToSet(ustensils, recipe.ustensils);
+    });
 
-        this.recipes.forEach(recipe => {
-            recipe.ingredients.forEach(ingredient => ingredients.add(ingredient.ingredient));
-            appliances.add(recipe.appliance);
-            recipe.ustensils.forEach(ustensil => ustensils.add(ustensil));
-        });
+    populateSelect('#ingredients-select', ingredients);
+    populateSelect('#appliances-select', appliances);
+    populateSelect('#ustensils-select', ustensils);
+};
 
-        this.populateSelect('#ingredients-select', ingredients);
-        this.populateSelect('#appliances-select', appliances);
-        this.populateSelect('#ustensils-select', ustensils);
-    }
+const addItemsToSet = (set, items, key) => {
+    items.forEach(item => set.add(key ? item[key].toLowerCase() : item.toLowerCase()));
+};
 
-    /**
-     * Populate a select element with items.
-     * @param {string} selectId - The ID of the select element.
-     * @param {Set} items - The items to populate the select with.
-     */
-    populateSelect(selectId, items) {
-        const select = document.querySelector(selectId);
-        select.innerHTML = '';
-        items.forEach(item => {
-            const option = document.createElement('option');
-            option.value = item;
-            option.textContent = item;
-            option.classList.add('py-2', 'capitalize-first-letter');
-            select.appendChild(option);
-        });
-    }
+/**
+ * Populate a select element with items.
+ * @param {string} selectId - The ID of the select element.
+ * @param {Set} items - The items to populate the select with.
+ */
+const populateSelect = (selectId, items) => {
+    const select = document.querySelector(selectId);
+    select.innerHTML = Array.from(items).map(item => `
+        <option value="${item}" class="py-2 capitalize-first-letter">${item}</option>
+    `).join('');
+};
 
-    /**
-     * Add event listeners for the select elements.
-     */
-    addSelectEventListeners() {
-        document.querySelector('#ingredients-select').addEventListener('change', (event) => {
-            this.addSelectedItem('ingredients', event.target.value);
+/**
+ * Add event listeners for the select elements.
+ */
+const addSelectEventListeners = () => {
+    ['ingredients', 'appliances', 'ustensils'].forEach(type => {
+        document.querySelector(`#${type}-select`).addEventListener('change', (event) => {
+            addSelectedItem(type, event.target.value);
             event.target.value = '';
-            this.filterRecipes(document.querySelector('#search').value);
+            filterRecipes(document.querySelector('#search').value);
         });
+    });
+};
 
-        document.querySelector('#appliances-select').addEventListener('change', (event) => {
-            this.addSelectedItem('appliances', event.target.value);
-            event.target.value = '';
-            this.filterRecipes(document.querySelector('#search').value);
-        });
+/**
+ * Add a selected item to the list of selected items.
+ * @param {string} type - The type of item (ingredients, appliances, ustensils).
+ * @param {string} value - The value of the item.
+ */
+const addSelectedItem = (type, value) => {
+    if (!value || selectedItems[type].has(value.toLowerCase())) return;
 
-        document.querySelector('#ustensils-select').addEventListener('change', (event) => {
-            this.addSelectedItem('ustensils', event.target.value);
-            event.target.value = '';
-            this.filterRecipes(document.querySelector('#search').value);
-        });
-    }
+    selectedItems[type].add(value.toLowerCase());
+    updateSelectedItemsDisplay();
+};
 
-    /**
-     * Add a selected item to the list of selected items.
-     * @param {string} type - The type of item (ingredients, appliances, ustensils).
-     * @param {string} value - The value of the item.
-     */
-    addSelectedItem(type, value) {
-        if (!value || this.selectedItems[type].has(value.toLowerCase())) return;
+/**
+ * Update the display of selected items.
+ */
+const updateSelectedItemsDisplay = () => {
+    const selectedItemsContainer = document.querySelector('#selected-items');
+    selectedItemsContainer.innerHTML = Object.keys(selectedItems).map(type => {
+        return Array.from(selectedItems[type]).map(item => `
+            <li class="flex w-48 items-center justify-between rounded-lg bg-color-site-50 p-4 text-sm">
+                ${escapeHtml(item)}
+                <button class="ml-2" onclick="removeSelectedItem('${type}', '${item}')">
+                    <i class="fas fa-times"></i>
+                </button>
+            </li>
+        `).join('');
+    }).join('');
+};
 
-        this.selectedItems[type].add(value.toLowerCase());
-        this.updateSelectedItemsDisplay();
-    }
+/**
+ * Remove a selected item from the list of selected items.
+ * @param {string} type - The type of item (ingredients, appliances, ustensils).
+ * @param {string} value - The value of the item.
+ */
+const removeSelectedItem = (type, value) => {
+    selectedItems[type].delete(value.toLowerCase());
+    updateSelectedItemsDisplay();
+    filterRecipes(document.querySelector('#search').value);
+};
 
-    /**
-     * Update the display of selected items.
-     */
-    updateSelectedItemsDisplay() {
-        const selectedItemsContainer = document.querySelector('#selected-items');
-        selectedItemsContainer.innerHTML = '';
+// Initialize the application
+main();
 
-        Object.keys(this.selectedItems).forEach(type => {
-            this.selectedItems[type].forEach(item => {
-                const listItem = document.createElement('li');
-                listItem.classList.add('flex', 'w-48', 'items-center', 'justify-between', 'rounded-lg', 'bg-color-site-50', 'p-4', 'text-sm');
-                listItem.innerHTML = `
-            ${escapeHtml(item)}
-            <button class="ml-2" onclick="app.removeSelectedItem('${type}', '${item}')">
-              <i class="fas fa-times"></i>
-            </button>
-          `;
-                selectedItemsContainer.appendChild(listItem);
-            });
-        });
-    }
-
-    /**
-     * Remove a selected item from the list of selected items.
-     * @param {string} type - The type of item (ingredients, appliances, ustensils).
-     * @param {string} value - The value of the item.
-     */
-    removeSelectedItem(type, value) {
-        this.selectedItems[type].delete(value.toLowerCase());
-        this.updateSelectedItemsDisplay();
-        this.filterRecipes(document.querySelector('#search').value);
-    }
-}
-
-const app = new App();
-app.main();
-
-// Pour rendre removeSelectedItem accessible depuis le HTML
-window.app = app;
+// Expose removeSelectedItem to the global scope
+window.removeSelectedItem = removeSelectedItem;
